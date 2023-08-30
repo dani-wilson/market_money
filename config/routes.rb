f90c5612652da85aaac03c727993fd06208427d4
Dani Wilson <danirae.powell@gmail.com>
2023-08-29 18:03:17 -0600

    add show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
-      resources :markets, only: [:index], controller: 'markets'
+      resources :markets, only: [:index, :show]
     end
   end
 end

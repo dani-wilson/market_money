19b69a916b9d048a1af07dd99bcccdd9a0166e69
Dani Wilson <danirae.powell@gmail.com>
2023-08-30 16:30:25 -0600

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
-      resources :markets, only: [:index, :show]
+      resources :markets, only: [:index, :show] do
+        resources :vendors, only: :index, to: 'market_vendors#index'
+      end
     end
   end
 end

ba57998b14af00edabb7f57b5226bca98a58ff19
Dani Wilson <danirae.powell@gmail.com>
2023-08-29 18:03:07 -0600

    add vendor_count to attributes

--- app/serializers/market_serializer.rb
+++ app/serializers/market_serializer.rb
@@ -1,17 +1,4 @@
 class MarketSerializer
-  def self.format_markets(markets)
-    markets.map do |market|
-      {
-        id: market.id,
-        name: market.name,
-        street: market.street,
-        city: market.city,
-        county: market.county,
-        state: market.state,
-        zip: market.zip,
-        lat: market.lat,
-        lon: market.lon
-      }
-    end
-  end
+  include JSONAPI::Serializer
+  attributes :name, :street, :city, :county, :state, :zip, :lat, :lon, :vendor_count
 end

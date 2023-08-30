153f57d0265c14e0dc4eff2572e1792f62437e01
Dani Wilson <danirae.powell@gmail.com>
2023-08-30 16:29:52 -0600

    add error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  def not_found_response(error)
+    error_object = Error.new(error.message, 404)
+    render json: ErrorSerializer.format_error(error), status: error_object.status_code
+  end
 end

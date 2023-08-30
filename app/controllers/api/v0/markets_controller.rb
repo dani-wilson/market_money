c425b26ca9a04c4081f32a68a2aedf2a8ffa24b1
Dani Wilson <danirae.powell@gmail.com>
2023-08-30 16:30:12 -0600

    remove error handling

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,13 +1,11 @@
 class Api::V0::MarketsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
   def index
     render json: MarketSerializer.new(Market.all)
   end
 
   def show
     render json: MarketSerializer.new(Market.find(params[:id]))
-  end
-
-  rescue_from ActiveRecord::RecordNotFound do |exception|
-    render json: { error: "Market not found" }, status: :not_found
   end
 end

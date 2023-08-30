753e946325c7299e9a8defe880e20ef807fec8e0
Dani Wilson <danirae.powell@gmail.com>
2023-08-30 16:30:54 -0600

    comment out validates inclusion of

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,10 +1,11 @@
 class Vendor < ApplicationRecord
-  validates_presence_of  :name,
+  validates_presence_of   :name,
                           :description,
                           :contact_name,
                           :contact_phone,
                           :credit_accepted
+  # validates_inclusion_of  :credit_accepted, in: [true, false]
 
-  has_many :market_vendors
+  has_many :market_vendors, dependent: :destroy
   has_many :markets, through: :market_vendors
 end

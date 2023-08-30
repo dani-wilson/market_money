8a2daadabdfa3856ef5c0e3ea82dc7344d09586a
Dani Wilson <danirae.powell@gmail.com>
2023-08-29 18:02:47 -0600

    add vendor_count method and attribute

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,13 +1,18 @@
 class Market < ApplicationRecord
-  validates_presence_of  :name,
+  validates_presence_of   :name,
                           :street,
                           :city,
                           :county,
                           :state,
                           :zip,
                           :lat,
-                          :lon
+                          :lon,
+                          :vendor_count
 
   has_many :market_vendors
   has_many :vendors, through: :market_vendors
+
+  def vendor_count
+    self.vendors.count
+  end
 end

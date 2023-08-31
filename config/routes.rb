0849e5f0b3bf00997934c7f01cdee7982e8595f7
Dani Wilson <danirae.powell@gmail.com>
2023-08-31 15:52:03 -0600

    add new and create routes for marketvendors

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
         resources :vendors, only: :index, to: 'market_vendors#index'
       end
       resources :vendors, only: [:show, :new, :create, :update, :destroy]
+      resources :market_vendors, only: [:new, :create]
     end
   end
 end

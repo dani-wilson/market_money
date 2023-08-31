ff28ab345eaa036611c4773c68a070ae69013579
Dani Wilson <danirae.powell@gmail.com>
2023-08-31 17:38:45 -0600

    add validations

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -1,4 +1,8 @@
 class MarketVendor < ApplicationRecord
+  validates :market_id, presence: true, allow_blank: false
+  validates :vendor_id, presence: true, allow_blank: false
+  validates :market_id, uniqueness: { scope: :vendor_id }
+
   belongs_to :vendor
   belongs_to :market
 end

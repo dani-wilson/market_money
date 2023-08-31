6727398e2d099fabf6a327392c7b02fe41f0ebf0
Dani Wilson <danirae.powell@gmail.com>
2023-08-31 14:04:57 -0600

    add patch route for vendors

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       resources :markets, only: [:index, :show] do
         resources :vendors, only: :index, to: 'market_vendors#index'
       end
-      resources :vendors, only: [:show, :new, :create]
+      resources :vendors, only: [:show, :new, :create, :update]
     end
   end
 end

4b6c52078adb165f37a34207697609ee7d49d969
Dani Wilson <danirae.powell@gmail.com>
2023-08-31 15:51:51 -0600

    begin create action

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -1,6 +1,5 @@
 class Api::V0::MarketVendorsController < ApplicationController
   before_action :find_market, only: :index
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
   def find_market 
     @market = Market.find(params[:market_id])
@@ -8,5 +7,15 @@ class Api::V0::MarketVendorsController < ApplicationController
 
   def index
     render json: VendorSerializer.new(@market.vendors)
+  end
+
+  def new
+  end
+
+  def create
+    @market = Market.find(params[:market_id])
+    @vendor = Vendor.find(params[:id])
+    new_market_vendor = MarketVendor.create(@market.id, @vendor.id)
+    render json: MarketVendorSerializer.new(@market, @vendor)
   end
 end

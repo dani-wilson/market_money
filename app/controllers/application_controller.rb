aae4ee2e0fb6b3ec9b45600dae015196b0aaec24
Dani Wilson <danirae.powell@gmail.com>
2023-08-31 11:32:23 -0600

    add rescue_from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :validation_error
+  
   def not_found_response(error)
     error_object = Error.new(error.message, 404)
     render json: ErrorSerializer.format_error(error), status: error_object.status_code

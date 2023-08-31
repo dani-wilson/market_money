2926861c9b634adc60373f4729c1c12fa35d6881
Dani Wilson <danirae.powell@gmail.com>
2023-08-31 14:04:09 -0600

    refactor error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::API
   
   def not_found_response(error)
     error_object = Error.new(error.message, 404)
-    render json: ErrorSerializer.format_error(error), status: error_object.status_code
+    render json: ErrorSerializer.serialize_json(error_object), status: error_object.status
   end
 
   def validation_error(error)
     error_object = Error.new(error.message, 400)
-    render json: ErrorSerializer.format_error(error), status: error_object.status_code
+    render json: ErrorSerializer.serialize_json(error_object), status: error_object.status
   end
 end

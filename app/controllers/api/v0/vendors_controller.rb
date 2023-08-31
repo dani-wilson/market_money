fe388e775fde6d12fcf839cf43fc61080c8ff8fc
Dani Wilson <danirae.powell@gmail.com>
2023-08-30 20:39:11 -0600

    add new, create actions as well as new rescue for validations

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -1,3 +1,20 @@
 class Api::V0::VendorsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :validation_error
+  
+  def show
+    render json: VendorSerializer.new(Vendor.find(params[:id]))
+  end
 
+  def new
+  end
+
+  def create
+    render json: VendorSerializer.new(Vendor.create!(new_vendor_params)), status: 201
+  end
+
+  private
+  def new_vendor_params
+    params.require(:vendor).permit(:name, :description, :contact_name, :contact_phone, :credit_accepted)
+  end
 end

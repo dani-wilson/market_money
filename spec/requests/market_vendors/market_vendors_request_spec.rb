c47365df1bc7c853a8d2ae3e4456f7c0b4b09d72
Dani Wilson <danirae.powell@gmail.com>
2023-08-31 17:39:26 -0600

    add tests for part of endpoint 9

--- spec/requests/market_vendors/market_vendors_request_spec.rb
+++ spec/requests/market_vendors/market_vendors_request_spec.rb
@@ -79,10 +79,151 @@ RSpec.describe "the market vendors index page" do
 
     MarketVendor.create!(market: market_1, vendor: vendor_1)
     MarketVendor.create!(market: market_1, vendor: vendor_2)
+
+    expect(MarketVendor.count).to eq(2)
+
+    market_vendor_params = { 
+      market_id: market_1.id,
+      vendor_id: vendor_3.id
+    }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v0/market_vendors", headers: headers, params: JSON.generate({market_vendor: market_vendor_params}) 
+
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+    expect(MarketVendor.count).to eq(3)
+
+    get "/api/v0/markets/#{market_1.id}/vendors"
+
+    expect(market_1.vendor_count).to eq(3)
+  end
+
+  it "will throw an error if an invalid vendor or market id is passed" do
+    market_1 = create(:market)
+
+    vendor_1 = create(:vendor)
+    vendor_2 = create(:vendor)
+    vendor_3 = create(:vendor)
+
+    MarketVendor.create!(market: market_1, vendor: vendor_1)
+    MarketVendor.create!(market: market_1, vendor: vendor_2)
+
+    market_vendor_params = { 
+      market_id: 47,
+      vendor_id: vendor_3.id
+    }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v0/market_vendors", headers: headers, params: JSON.generate({market_vendor: market_vendor_params})
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+
+    market_vendor = JSON.parse(response.body, symbolize_names: true)
+    expect(market_vendor[:errors][0][:title]).to eq("Validation failed: Market must exist")
+
+    market_vendor_params = { 
+      market_id: market_1.id,
+      vendor_id: 87
+    }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v0/market_vendors", headers: headers, params: JSON.generate({market_vendor: market_vendor_params})
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+
+    market_vendor = JSON.parse(response.body, symbolize_names: true)
+    expect(market_vendor[:errors][0][:title]).to eq("Validation failed: Vendor must exist")
+  end
+
+  it "will throw an error if a vendor or market id is NOT passed in" do
+    market_1 = create(:market)
+
+    vendor_1 = create(:vendor)
+    vendor_2 = create(:vendor)
+    vendor_3 = create(:vendor)
+
+    MarketVendor.create!(market: market_1, vendor: vendor_1)
+    MarketVendor.create!(market: market_1, vendor: vendor_2)
+
+    market_vendor_params = { 
+      market_id: "",
+      vendor_id: vendor_3.id
+    }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v0/market_vendors", headers: headers, params: JSON.generate({market_vendor: market_vendor_params})
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+
+    market_vendor = JSON.parse(response.body, symbolize_names: true)
+    expect(market_vendor[:errors][0][:title]).to eq("Validation failed: Market can't be blank, Market must exist")
+
+    market_vendor_params = { 
+      market_id: market_1.id,
+      vendor_id: ""
+    }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v0/market_vendors", headers: headers, params: JSON.generate({market_vendor: market_vendor_params})
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+
+    market_vendor = JSON.parse(response.body, symbolize_names: true)
+    expect(market_vendor[:errors][0][:title]).to eq("Validation failed: Vendor can't be blank, Vendor must exist")
+  end
+
+  it "will throw an error if the market or vendor id already exists" do
+    market_1 = create(:market)
+
+    vendor_1 = create(:vendor)
+    vendor_2 = create(:vendor)
+    vendor_3 = create(:vendor)
+
+    MarketVendor.create!(market: market_1, vendor: vendor_1)
+    MarketVendor.create!(market: market_1, vendor: vendor_2)
+
+    market_vendor_params = { 
+      market_id: market_1.id,
+      vendor_id: vendor_1.id
+    }
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v0/market_vendors", headers: headers, params: JSON.generate({market_vendor: market_vendor_params})
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+    
+    market_vendor = JSON.parse(response.body, symbolize_names: true)
+    expect(market_vendor[:errors][0][:title]).to eq("Validation failed: Market has already been taken")
+  end
+
+  it "can delete a marketvendor" do
+    market_1 = create(:market)
+
+    vendor_1 = create(:vendor)
+    vendor_2 = create(:vendor)
+    vendor_3 = create(:vendor)
+
+    MarketVendor.create!(market: market_1, vendor: vendor_1)
+    MarketVendor.create!(market: market_1, vendor: vendor_2)
     MarketVendor.create!(market: market_1, vendor: vendor_3)
 
-    post "/api/v0/market_vendors"
+    expect(MarketVendor.count).to eq(3)
 
-    
+    market_vendor_params = ({
+      market_id: market_1.id,
+      vendor_id: vendor_3.id
+    })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    delete "/api/v0/market_vendors/", headers: headers, params: JSON.generate(market_vendor: market_vendor_params)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(204)
   end
 end

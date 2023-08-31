78b5279622c0867f47d046707859fe17b75c7076
Dani Wilson <danirae.powell@gmail.com>
2023-08-30 20:40:00 -0600

    edit contact phone faker

--- spec/factories/vendors.rb
+++ spec/factories/vendors.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     name { "#{ Faker::Dessert.flavor } #{ Faker::Ancient.primordial } #{ Faker::Creature::Animal.name.capitalize }" }
     description { Faker::TvShows::NewGirl.quote }
     contact_name { Faker::Name.unique.name }
-    contact_phone { Faker::PhoneNumber.unique }
+    contact_phone { Faker::PhoneNumber.cell_phone }
     credit_accepted { Faker::Boolean }
   end
 end

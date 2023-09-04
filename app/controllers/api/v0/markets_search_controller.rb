f5d99e741d64ff15589a5c7343195092afc70b20
Dani Wilson <danirae.powell@gmail.com>
2023-09-04 15:15:04 -0600

    add private params for search function

--- app/controllers/api/v0/markets_search_controller.rb
+++ app/controllers/api/v0/markets_search_controller.rb
@@ -1,6 +1,29 @@
 class Api::V0::MarketsSearchController < ApplicationController
+  before_action :validate_params, only: :index
+
   def index
     @markets = MarketsFacade.search_markets(params[:name], params[:city], params[:state])
     render json: MarketSerializer.new(@markets)
+  end
+
+  private
+
+  def validate_params
+    if search_city? || search_city_and_name?
+      params_invalid_response
+    end
+  end
+
+  def search_city?
+    params[:city].present? && !params[:name].present? && !params[:state].present?
+  end
+
+  def search_city_and_name?
+    params[:city].present? && params[:name].present? && !params[:state].present?
+  end
+
+  def params_invalid_response
+    error_object = Error.new("Invalid parameters.", 422)
+    render json: ErrorSerializer.serialize_json(error_object), status: 422
   end
 end
